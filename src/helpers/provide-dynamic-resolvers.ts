7485455a27fdb503de3db3cca86178a47f9b795a
Tarık İnce <incetarik@hotmail.com>
2022-04-27 04:36:30 +0300

    Package version upgrade

--- src/helpers/provide-dynamic-resolvers.ts
+++ src/helpers/provide-dynamic-resolvers.ts
@@ -1,4 +1,4 @@
-import { Provider, Type } from '@nestjs/common'
+import { Type } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 
 import { SYM_PRISMA_CLIENT } from '../constants'
@@ -6,7 +6,7 @@ import { generateDynamicResolvers } from '../dynamic-resolvers'
 import { flattenArray } from './flatten-array'
 
 /**
- * Returns the {@link Provider} array of dynamic resolvers.
+ * Returns the {@link Type} array of dynamic resolvers.
  *
  * @export
  * @param {Type<PrismaClient>} prismaService The prisma service used for
@@ -15,11 +15,11 @@ import { flattenArray } from './flatten-array'
  * @param {string} [moduleName='_global'] The name of the module which will be
  * loaded.
  * 
- * @return {Provider<any>[]} The array of dynamic resolvers.
+ * @return {Type<any>[]} The array of dynamic resolvers.
  */
-export function provideDynamicResolvers(prismaService: Type<PrismaClient>, moduleName = '_global'): Provider<any>[] {
+export function provideDynamicResolvers(prismaService: Type<PrismaClient>, moduleName = '_global'): Type<any>[] {
   return [
-    { provide: SYM_PRISMA_CLIENT, useExisting: prismaService },
+    { provide: SYM_PRISMA_CLIENT, useExisting: prismaService } as any,
     ...buildDynamicResolversArray(moduleName)
   ]
 }
@@ -34,5 +34,3 @@ function buildDynamicResolversArray(moduleName: string) {
 
   return resolvers
 }
-
-
