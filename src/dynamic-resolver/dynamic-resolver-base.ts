e42184142845791f4a6b5532c9d96c15e1c02e38
Tarık İnce <incetarik@hotmail.com>
2022-08-21 13:18:38 +0300

    Removed unused methods in `DynamicResolverBase`

--- src/dynamic-resolver/dynamic-resolver-base.ts
+++ src/dynamic-resolver/dynamic-resolver-base.ts
@@ -1,81 +1,9 @@
-import { GraphQLResolveInfo } from 'graphql'
-
-import { getNavigationMaps, INavigationMap } from '../dynamic-navigations'
-import {
-  extractGraphQLSelectionPath,
-  extractGraphQLSelections,
-  toCamelCase,
-  updatePathInfoFromSchema,
-} from '../helpers'
-
+import type { GraphQLResolveInfo } from 'graphql'
+import type { INavigationMap } from '../dynamic-navigations'
 import type { Dictionary } from '../types'
 
 export abstract class DynamicResolverBase {
   constructor(protected readonly prisma: any) {}
-
-  protected getSelectionObject(info: GraphQLResolveInfo, selectionMap?: Dictionary<string>) {
-    const select = extractGraphQLSelections({
-      node: info.fieldNodes[ 0 ],
-      selectionMap,
-    })
-
-    return select
-  }
-
-  protected getSelectionPath(info: GraphQLResolveInfo, selectionMap?: Dictionary<string>) {
-    const selectionPath = extractGraphQLSelectionPath({
-      path: info.path,
-    })
-
-    const pathInfoList = selectionPath
-    pathInfoList.forEach(pathInfo => {
-      if (pathInfo.table) return
-
-      const tableName = toCamelCase(pathInfo.tableName)
-      const maps = getNavigationMaps()
-        .filter(it => it.sourceTableName === tableName)
-
-      pathInfo.table = maps[ 0 ]?.source
-      pathInfo.navigationMaps = maps
-
-      updatePathInfoFromSchema(pathInfo, info.schema)
-    })
-
-    const query = pathInfoList[ 0 ]
-
-    if (pathInfoList.length < 3) {
-      throw new Error(`The navigation source could not be extracted in (${query.propertyName})`)
-    }
-
-    const firstLink = pathInfoList[ 1 ]
-
-    if (!firstLink.table) {
-      throw new Error(`The navigation target could not be extracted in (${query.propertyName}): '${firstLink.propertyName}' → ?`)
-    }
-
-    const secondLink = pathInfoList[ 2 ]
-
-    if (!secondLink.table) {
-      throw new Error(`The navigation target could not be extracted in (${query.propertyName}): ${firstLink.propertyName} → ${secondLink.propertyName}' → ?`)
-    }
-
-    const sourceTableName
-      = selectionMap?.[ firstLink.propertyName ]
-      ?? toCamelCase(firstLink.table.name)
-
-    const source = this.prisma[ sourceTableName ]
-    if (!source) {
-      throw new Error(`Unrecognized Prisma model: "${sourceTableName}". Failed to build resolver for navigation: '${sourceTableName} → ${secondLink.propertyName}'`)
-    }
-
-    return {
-      to: secondLink,
-      root: query,
-      from: firstLink,
-      source,
-      selectionPath,
-    }
-  }
 
   protected resolve(parent: any, _primaryKeyName: string, _info: GraphQLResolveInfo, navigationMap: INavigationMap, _selectionMap?: Dictionary<string>) {
     const {
